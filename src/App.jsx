45296aacd4cee8ec92e043cba94d2b0a18a2a5dc
marteloviseh.s <marte.lovise.hillestad@gmail.com>
2026-09-04 15:44:44 +0200

    style på knapper

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ function App() {
   const [valgTid, setValgtTid] = useState(0);
   const [valgEnerginiva, setValgtEnerginiva] = useState();
   const [resultat, setResultat] = useState([]);
- 
+  const [steg, setSteg] = useState("energi");
+
   function finnOkt  () {
     const nyOkt = velgOkt(valgTid, valgEnerginiva);
     setResultat(nyOkt);
@@ -20,17 +21,54 @@ function App() {
 
 
   return (
-    <div>
-      <p> Valgt tid: {valgTid}</p>
-      <button onClick={() => setValgtTid(30)}>30 minutter</button>
-      <button onClick={() => setValgtTid(60)}>60 minutter</button>
-      <button onClick={() => setValgtTid(120)}>120 minutter</button>
 
-      <p> Velg energinivå: {valgEnerginiva}</p>
-      <button onClick={() => setValgtEnerginiva(ENERGINIVA.LAV)}>Lav</button>
-      <button onClick={() => setValgtEnerginiva(ENERGINIVA.MIDDELS)}>Middels</button>
-      <button onClick={() => setValgtEnerginiva(ENERGINIVA.HOY)}>Høy</button>
-      <button onClick={finnOkt}>Finn økt</button>
+<div>
+    {steg === "energi" && (
+      <div>
+        <p>Hvordan føles energinivået i dag?</p>
+        <button className="knapp-lav" onClick={() => { setValgtEnerginiva(ENERGINIVA.LAV); setSteg("tid"); }}>
+        😣 Lav
+        </button>
+        <button className="knapp-middels" onClick={() => { setValgtEnerginiva(ENERGINIVA.MIDDELS); setSteg("tid"); }}>
+        😐 Middels
+        </button>
+        <button className="knapp-hoy" onClick={() => { setValgtEnerginiva(ENERGINIVA.HOY); setSteg("tid"); }}>
+        😊💪 Høy
+        </button>
+
+      </div>
+    )}
+
+    {steg === "tid" && (
+      <div>
+        <p>Hvor lang tid har du til å trene?</p>
+        <button className="knapp-30" onClick={() => {setValgtTid(30); setSteg("resultat"); }}>30 minutter</button>
+
+        <button className="knapp-60" onClick={() => {setValgtTid(60); setSteg("resultat"); }}>60 minutter</button>
+
+        <button className="knapp-120" onClick={() => {setValgtTid(120); setSteg("resultat"); }}>120 minutter</button>
+      </div>
+    )}
+      {steg === "resultat" && (
+
+      <div>
+        <button className="knapp-resultat" onClick={finnOkt}>Dagens økt</button>
+        <ul>
+          {resultat.map((ovelse) => (
+            <li key={ovelse.id}>{ovelse.navn} - {ovelse.tid} min </li>
+          ))}
+        </ul>
+      </div>
+    )
+      }
+
+
+    
+
+
+
+  
+     
 
       <ul>
         {resultat.map((ovelse) => (
